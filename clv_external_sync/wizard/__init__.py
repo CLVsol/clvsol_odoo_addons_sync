7d3b3187da8e51125a0ecc22c450ac3fa363071f
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-06-26 09:58:10 -0300

    [IMP] clv_external_sync (ADD wizard/external_sync_schedule_exec_2)

--- clv_external_sync/wizard/__init__.py
+++ clv_external_sync/wizard/__init__.py
@@ -6,6 +6,7 @@ from . import external_sync_mass_edit
 from . import external_sync_template_mass_edit
 from . import external_sync_schedule_mass_edit
 from . import external_sync_schedule_exec
+from . import external_sync_schedule_exec_2
 from . import external_sync_batch_exec
 from . import external_sync_batch_member_mass_edit
 from . import external_sync_schedule_mass_edit_2
