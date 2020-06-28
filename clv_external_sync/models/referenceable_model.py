6aa4118866b605d246eb83890d2c731aa475000f
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-06-28 17:48:26 -0300

    [IMP] clv_external_sync (IMP referenceable_mode)

--- clv_external_sync/models/referenceable_model.py
+++ clv_external_sync/models/referenceable_model.py
@@ -10,9 +10,45 @@ class ExternalSyncBatchMember(models.Model):
     _inherit = 'clv.external_sync.batch.member', 'clv.abstract.reference'
 
     ref_method = fields.Char(
-        string='Model',
+        string='Method',
         compute='_compute_refenceable_model_schedule',
         store=True
+    )
+
+    ref_disable_identification = fields.Boolean(
+        string='Disable Identification',
+        compute='_compute_refenceable_model_schedule',
+        store=True
+    )
+    ref_disable_identification_suport = fields.Boolean(
+        string='Disable Identification:',
+        compute='_compute_refenceable_model_schedule',
+        store=False,
+        compute_sudo=True
+    )
+
+    ref_disable_inclusion = fields.Boolean(
+        string='Disable Inclusion',
+        compute='_compute_refenceable_model_schedule',
+        store=True
+    )
+    ref_disable_inclusion_suport = fields.Boolean(
+        string='Disable Inclusion:',
+        compute='_compute_refenceable_model_schedule',
+        store=False,
+        compute_sudo=True
+    )
+
+    ref_disable_sync = fields.Boolean(
+        string='Disable Sync',
+        compute='_compute_refenceable_model_schedule',
+        store=True
+    )
+    ref_disable_sync_suport = fields.Boolean(
+        string='Disable Sync:',
+        compute='_compute_refenceable_model_schedule',
+        store=False,
+        compute_sudo=True
     )
 
     @api.depends('ref_id')
@@ -21,5 +57,17 @@ class ExternalSyncBatchMember(models.Model):
             try:
                 if record.ref_id:
                     record.ref_method = record.ref_id.method
+                    record.ref_disable_identification = record.ref_id.disable_identification
+                    record.ref_disable_inclusion = record.ref_id.disable_inclusion
+                    record.ref_disable_sync = record.ref_id.disable_sync
+                    record.ref_disable_identification_suport = record.ref_id.disable_identification
+                    record.ref_disable_inclusion_suport = record.ref_id.disable_inclusion
+                    record.ref_disable_sync_suport = record.ref_id.disable_sync
             except Exception:
                 record.ref_method = False
+                record.ref_disable_identification = False
+                record.ref_disable_inclusion = False
+                record.ref_disable_sync = False
+                record.ref_disable_identification_suport = False
+                record.ref_disable_inclusion_suport = False
+                record.ref_disable_sync_suport = False
