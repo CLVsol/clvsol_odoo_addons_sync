1a0e6ab10bbeb7e57ea315f20783b7279eb2decc
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-07-10 08:40:32 -0300

    [IMP] clv_external_sync (ADD data/global_log_client.xml)

--- clv_external_sync/__manifest__.py
+++ clv_external_sync/__manifest__.py
@@ -40,6 +40,7 @@
         'wizard/external_sync_batch_exec_view.xml',
         'wizard/external_sync_batch_member_mass_edit_view.xml',
         'wizard/external_sync_schedule_mass_edit_2_view.xml',
+        'data/global_log_client.xml',
     ],
     'demo': [],
     'test': [],
