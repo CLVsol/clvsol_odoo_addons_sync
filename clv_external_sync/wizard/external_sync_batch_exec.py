910ab14ffdc57b53df88d5f95d46c811992fcf6c
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-11-10 14:56:44 -0300

    [IMP] clv_external_sync (IMP external_sync_batch)

--- clv_external_sync/wizard/external_sync_batch_exec.py
+++ clv_external_sync/wizard/external_sync_batch_exec.py
@@ -31,13 +31,11 @@ class ExternalSyncBatchExec(models.TransientModel):
         store=False
     )
 
-    # @api.multi
     @api.depends('batch_ids')
     def _compute_count_batches(self):
         for r in self:
             r.count_batches = len(r.batch_ids)
 
-    # @api.multi
     def _reopen_form(self):
         self.ensure_one()
         action = {
@@ -50,75 +48,15 @@ class ExternalSyncBatchExec(models.TransientModel):
         }
         return action
 
-    # @api.multi
     def do_external_sync_batch_exec(self):
         self.ensure_one()
-
-        from time import time
-        start = time()
-
-        sync_log = False
 
         for batch in self.batch_ids:
 
             _logger.info(u'%s %s', '>>>>>', batch.name)
 
-            if sync_log is False:
-                sync_log = '########## ' + batch.name + ' ##########\n'
-            else:
-                sync_log += '\n########## ' + batch.name + ' ##########\n'
-
-            for external_sync_batch_member in batch.external_sync_batch_member_ids:
-
-                if external_sync_batch_member.enabled:
-
-                    schedule = external_sync_batch_member.ref_id
-
-                    _logger.info(u'%s %s', '>>>>>', schedule.name)
-
-                    model = schedule.model
-                    _logger.info(u'%s %s [%s]', '>>>>>', schedule.name, model)
-
-                    # method_call = False
-                    # if schedule.method == '_object_external_sync':
-                    #     method_call = 'self.env["clv.external_sync"].' + schedule.method + '(schedule)'
-                    # elif schedule.method == '_object_external_recognize':
-                    #     method_call = 'self.env["clv.external_sync"].' + schedule.method + '(schedule)'
-
-                    method_call = 'self.env["clv.external_sync"].' + schedule.method + '(schedule)'
-
-                    _logger.info(u'%s %s %s', '>>>>>>>>>>', schedule.method, method_call)
-
-                    if method_call:
-
-                        schedule.sync_log = 'method: ' + str(schedule.method) + '\n\n'
-                        schedule.sync_log +=  \
-                            'external_host: ' + str(schedule.external_host_id.name) + '\n' + \
-                            'external_dbname: ' + str(schedule.external_host_id.external_dbname) + '\n\n' + \
-                            'max_task: ' + str(schedule.max_task) + '\n' + \
-                            'enable_identification: ' + \
-                            str(schedule.enable_identification) + '\n' + \
-                            'enable_check_missing: ' + \
-                            str(schedule.enable_check_missing) + '\n' + \
-                            'enable_inclusion: ' + str(schedule.enable_inclusion) + '\n' + \
-                            'enable_sync: ' + str(schedule.enable_sync) + '\n' + \
-                            'external_last_update_args: ' + str(schedule.external_last_update_args()) + '\n\n' + \
-                            'enable_sequence_code_sync: ' + str(schedule.enable_sequence_code_sync) + '\n\n'
-
-                        exec(method_call)
-
-                    sync_log += '\n########## ' + schedule.name + ' ##########\n'
-                    sync_log += schedule.sync_log
-
-                    self.env.cr.commit()
-
-            sync_log += '\n############################################################'
-            sync_log +=  \
-                '\nExecution time: ' + str(secondsToStr(time() - start)) + '\n'
-
-            batch.sync_log = sync_log
-
-            _logger.info(u'%s %s', '>>>>> Execution time: ', secondsToStr(time() - start))
+            ExternalSyncBatch = self.env['clv.external_sync.batch']
+            ExternalSyncBatch._external_sync_batch_exec(batch.name)
 
         return True
         # return self._reopen_form()
